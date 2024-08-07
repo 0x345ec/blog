6daa09eccc1b47fcd16ea55f76eb6eb300a1859e
hyprpwn <staff@34sec.org>
2024-08-07 16:39:15 +0200

    fixedurl

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,8 +6,8 @@ const siteMetadata = {
   description: 'Home of our papers and projects',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  // siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  // siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
   // mastodon: 'https://mastodon.social/@mastodonuser',
